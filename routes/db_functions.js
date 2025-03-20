e592d69fb603b58aaa602a94d10fc8c687616242
shoham_z <shozeh123@gmail.com>
2025-03-21 00:00:57 +0200

    added more basic functionality

---
+++ routes/db_functions.js
@@ -0,0 +1,15 @@
+function addSwitch(ip, name){
+
+}
+
+function editSwitch(ip, name){
+
+}
+
+function deleteSwitch(ip){
+
+}
+
+module.exports = {addSwitch,
+    editSwitch,
+    deleteSwitch}
--- routes/switch.js
+++ routes/switch.js
@@ -1,33 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const {
-    getUsers, getUserPhone, changPassword, deleteUser,
-} = require("../dbBridge");
 const {responseGet} = require("../headers");
+const { addSwitch, editSwitch, deleteSwitch } = require("./db_functions");
 
 /* GET users section. */
 router.get("/", function (req, res, _next) {
-    getUsers().then((data) => responseGet(res, data));
+    //get all switches
 });
 
-router.get("/phone", function (req, res, _next) {
-    getUserPhone(req.query.username).then((data) => responseGet(res, data));
+router.get("/:switchIp", function (req, res, _next) {
+    const switchIp = req.params.switchIp;
+    responseGet(res, "get " + req.params.switchIp);
 });
 
-router.post("/change-password", (req, res) => {
-    console.log(req.body.userPhone, req.body.newPassword);
-    changPassword(req.body.userPhone, req.body.newPassword)
-        .then((response) => {
-            if (response === 0) res.status(200).json({message: "changed successfully"});
-        })
-        .catch((err) => console.log(err));
+router.post("/add/", (req, res) => {
+    const {ip, name} = req.body;
+    addSwitch(ip, name).then(()=> responseGet(res, "added " + ip));
 });
 
-router.delete("/delete-user", (req, res) => {
-    let response = deleteUser(req.body.userPhone);
+router.put("/edit/", (req, res) => {
+    const {ip, name} = req.body;
+    editSwitch(ip, name).then(()=> responseGet(res, "edited " + ip));
+});
 
-    if (response === 0) res.status(200);
-
+router.delete("/remove/:switchIp", (req, res) => {
+    const ip = req.params.switchIp;
+    deleteSwitch(ip).then(()=> responseGet(res, "removed " + ip));
+    //responseGet(res, "delete " + req.params.switchIp);
+    // delete switch
 });
 
 module.exports = router;
