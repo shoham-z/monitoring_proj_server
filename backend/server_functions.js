b7e918175e69b3a6441b5f803bcbb8a1d5f4ea45
YTG667 <patrick667tnt@gmail.com>
2026-02-03 09:14:33 +0200

    Changed the format of backup names to not include seconds in it's name

--- backend/server_functions.js
+++ backend/server_functions.js
@@ -501,8 +501,7 @@ async function backupDatabase() {
       month: '2-digit', 
       day: '2-digit', 
       hour: '2-digit', 
-      minute: '2-digit', 
-      second: '2-digit', 
+      minute: '2-digit',  
       timeZone: 'Asia/Jerusalem' 
   }).replace(/\//g, '-').replace(/, /g, '_').replace(/:/g, '-')}.db`;
 
